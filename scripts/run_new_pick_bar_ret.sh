c1565062d7ee6b625d84149a30aa5132e8326087
Yu-Hsiang-Fu <seanfu920506@gmail.com>
2025-01-17 13:01:28 +0800

    mod

--- scripts/run_new_pick_bar_ret.sh
+++ scripts/run_new_pick_bar_ret.sh
@@ -1,6 +1,6 @@
 python main_exp.py -s 400 --n 1 --retrieve_on --refine_step 0 \
---out_file "results_new/new_pick_bar_rej.json" \
---out_dir "results_new/new_pick_bar_rej" \
+--out_file "results_new/new_pick_bar_ret.json" \
+--out_dir "results_new/new_pick_bar_ret" \
 --temperature 0.3 \
 --agg_metric "min" \
 --enc_method "dinov2" \
